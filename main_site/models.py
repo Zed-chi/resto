1037115852a916578f03d01be62b3aba3ef4218f
zed-chi <zed-chi@mail.ru>
2023-01-17 22:57:28 +0300

    added string representation

--- main_site/models.py
+++ main_site/models.py
@@ -6,16 +6,25 @@ from cookery.models import Dish
 class Room(models.Model):
     title = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.title
+
 
 class RoomTable(models.Model):
     room = models.ForeignKey(
         Room, on_delete=models.CASCADE, related_name="tables"
     )
-    number = models.IntegerField()
+    number = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return f"{self.room.title} - {self.number}"
 
 
 class Menu(models.Model):
     title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
 
 
 class MenuCategory(models.Model):
@@ -24,11 +33,18 @@ class MenuCategory(models.Model):
         Menu, on_delete=models.CASCADE, related_name="categories"
     )
 
+    def __str__(self):
+        return f"{self.menu.title} - {self.title}"
+
 
 class MenuItem(models.Model):
+    title = models.CharField(max_length=255)
     dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
     category = models.ForeignKey(
         MenuCategory, on_delete=models.CASCADE, related_name="items"
     )
     price = models.DecimalField(decimal_places=2, max_digits=10)
-    is_available = models.BooleanField()
+    is_available = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.category.id} - {self.title}"
