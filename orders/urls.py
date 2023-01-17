04f7f054707803bdf65fc29d03b2a55f98f16ba0
zed-chi <zed-chi@mail.ru>
2023-01-17 22:56:28 +0300

    base order routes

--- orders/urls.py
+++ orders/urls.py
@@ -3,5 +3,7 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="index"),
+    path("new/<table_pk>", views.new_order, name="new_order"),
+    # path("waitlist/", views.index, name="waitlist"),
 ]
