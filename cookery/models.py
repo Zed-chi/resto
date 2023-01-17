412a23192c2a9f20040726bfcdd27e550702f7fb
zed-chi <zed-chi@mail.ru>
2023-01-17 22:54:56 +0300

    cook models reorganization

--- cookery/models.py
+++ cookery/models.py
@@ -7,28 +7,15 @@ class Dish(models.Model):
     def get_total_callories(self):
         pass
 
-
-class Recipe(models.Model):
-    title = models.CharField(max_length=255)
-    dish = models.ForeignKey(
-        Dish, on_delete=models.CASCADE, related_name="recipe"
-    )
-
-
-class RecipeStep(models.Model):
-    recipe = models.ForeignKey(
-        Recipe, on_delete=models.CASCADE, related_name="steps"
-    )
-    description = models.TextField()
-    image = models.ImageField()
+    def __str__(self):
+        return self.title
 
 
 class Ingredient(models.Model):
     title = models.CharField(max_length=255)
-    weight = models.DecimalField(decimal_places=3, max_digits=6)
 
-    def get_callories(self):
-        pass
+    def __str__(self):
+        return self.title
 
 
 class CalorieItem(models.Model):
@@ -36,3 +23,39 @@ class CalorieItem(models.Model):
         Ingredient, on_delete=models.CASCADE, related_name="calorie"
     )
     by_weight = models.DecimalField(decimal_places=3, max_digits=6)
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+    dish = models.ForeignKey(
+        Dish, on_delete=models.CASCADE, related_name="recipe"
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class RecipeIngridient(models.Model):
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name="ingridients"
+    )
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    weight = models.DecimalField(decimal_places=3, max_digits=6)
+
+    def get_callories(self):
+        pass
+
+    def __str__(self):
+        return f"{self.ingredient.title} №{self.order_num}"
+
+
+class RecipeStep(models.Model):
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name="steps"
+    )
+    description = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+    order_num = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.recipe.title} №{self.order_num}"
