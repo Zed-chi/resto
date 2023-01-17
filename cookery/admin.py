795e3a9ec6444796b7fa30b8651d0852b918d6ba
zed-chi <zed-chi@mail.ru>
2023-01-17 22:54:38 +0300

    models from cook page added to admin page

--- cookery/admin.py
+++ cookery/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CalorieItem, Dish, Ingredient, Recipe, RecipeStep
+
+admin.site.register(Recipe)
+admin.site.register(Dish)
+admin.site.register(RecipeStep)
+admin.site.register(Ingredient)
+admin.site.register(CalorieItem)
