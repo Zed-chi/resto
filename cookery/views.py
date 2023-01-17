f7238acbc5cf9512867dce9af462232406cd8960
zed-chi <zed-chi@mail.ru>
2023-01-17 22:58:13 +0300

    cook views

--- cookery/views.py
+++ cookery/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from orders.models import OrderItem
+
+from .models import Recipe
 
 
 # Create your views here.
-def index(req):
-    return render(req, "cooklist.html")
+def order_items_list(req):
+    items = OrderItem.objects.filter(order__is_completed=False)
+    return render(req, "orderlist.html", {"items": items})
+
+
+def recipe_detail(req, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    return render(request, "recipe.html", {"recipe": recipe})
+
+
+def order_item_detail(request, pk):
+    item = get_object_or_404(OrderItem, pk=pk)
+    return render(request, "recipe.html", {"recipe": recipe})
+
+
+def order_item_cooked(request, pk):
+    item = get_object_or_404(OrderItem, pk=pk)
+    return render(request, "recipe.html", {"recipe": recipe})
