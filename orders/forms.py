53f7c00731caaa71fc7e5f7a48c6734ebbd99c83
zed-chi <zed-chi@mail.ru>
2023-01-19 16:27:50 +0300

    changed inline formset idea, to simple form one

--- orders/forms.py
+++ orders/forms.py
@@ -6,24 +6,21 @@ from .models import Order, OrderItem
 
 
 class OrderItemForm(forms.ModelForm):
+    quantity = forms.IntegerField(min_value=1, initial=1)
+
     class Meta:
         model = OrderItem
-        fields = ["menu_item", "order", "notes", "quantity"]
-
-    def save(self, commit=True):
-        obj = super().save(commit=False)
-        obj.price = obj.menu_item.price
-        if commit:
-            obj.save()
-        return obj
+        fields = ["menu_item", "notes", "quantity"]
+        widgets = {
+            "menu_item": forms.Select(attrs={"class": "form-control"}),
+            "notes": forms.Textarea(
+                attrs={"class": "form-control", "rows": "2"}
+            ),
+            "quantity": forms.NumberInput(attrs={"class": "form-control"}),
+        }
 
 
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ["table"]
-
-
-OrderItemInlineFormSet = inlineformset_factory(
-    Order, OrderItem, form=OrderItemForm
-)
+        exclude = ["table", "is_completed", "is_draft"]
