19765b8d9c35954bd1242c0a64763a3427d2bc0a
zed-chi <zed-chi@mail.ru>
2023-01-19 16:28:57 +0300

    changed routes

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,19 @@
-from django.urls import include, path
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
+    path("waitlist/", views.waitlist, name="waitlist"),
+    path(
+        "table/<table_id>", views.table_orders_list, name="table_orders_list"
+    ),
+    path("<order_id>/pay", views.pay_order, name="pay_order"),
+    path("<order_id>/remove", views.remove_order, name="remove_order"),
+    path(
+        "<order_id>/cook",
+        views.transfer_order_to_cook,
+        name="transfer_order_to_cook",
+    ),
+    path("<order_id>", views.order_item_create, name="order_item_create"),
     path("", views.index, name="index"),
-    path("new/<table_pk>", views.new_order, name="new_order"),
-    # path("waitlist/", views.index, name="waitlist"),
 ]
