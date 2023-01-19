465e2c14fd4d61733c5cc99537ffe34891a0d037
zed-chi <zed-chi@mail.ru>
2023-01-20 00:28:40 +0300

    route upd

--- cookery/views.py
+++ cookery/views.py
@@ -8,19 +8,19 @@ from .models import Recipe
 # Create your views here.
 def order_items_list(req):
     items = OrderItem.objects.filter(order__is_completed=False)
-    return render(req, "orderlist.html", {"items": items})
+    return render(req, "cook/orderlist.html", {"items": items})
 
 
 def recipe_detail(req, pk):
     recipe = get_object_or_404(Recipe, pk=pk)
-    return render(request, "recipe.html", {"recipe": recipe})
+    return render(request, "cook/recipe.html", {"recipe": recipe})
 
 
 def order_item_detail(request, pk):
     item = get_object_or_404(OrderItem, pk=pk)
-    return render(request, "recipe.html", {"recipe": recipe})
+    return render(request, "cook/recipe.html", {"recipe": recipe})
 
 
 def order_item_cooked(request, pk):
     item = get_object_or_404(OrderItem, pk=pk)
-    return render(request, "recipe.html", {"recipe": recipe})
+    return render(request, "cook/recipe.html", {"recipe": recipe})
