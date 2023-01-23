e3f665b592c0fdefa2f68df709b09285b4560c16
zed-chi <zed-chi@mail.ru>
2023-01-23 23:21:23 +0300

    fix

--- main_site/views.py
+++ main_site/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 from kitchen.models import Menu
 
 
-# Create your views here.
 def index(req):
     menus = Menu.objects.all()
     return render(req, "main/index.html", {"menus": menus})
--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import views as auth
+# from django.contrib.auth import views as auth
 from django.urls import include, path
 
 from . import views
